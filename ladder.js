b4a577e8d0d9205f4b4eb4278896c4c3854cb27f
Rose <apontera2111@aguafria.org>
2021-04-19 13:31:55 -0700

    1

---
+++ ladder.js
@@ -0,0 +1,18 @@
+let ladder = {
+  position: 0,
+  height: 0,
+  up() {
+    this.position += 1;
+  },
+  down() {
+    this.position -= 1;
+  },
+  showStep() {
+    return this.position;
+  },
+};
+
+ladder.up();
+ladder.up();
+ladder.down();
+alert(`Current step is: ${ladder.showStep()}`);
